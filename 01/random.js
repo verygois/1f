bb6d65ac24413d84d3f4dcfbe6d0d98fe0b8a378
Very Gois <we.are.pe.hu@gmail.com>
2021-08-24 13:51:53 +0900

    01

---
+++ 01/random.js
@@ -0,0 +1,22 @@
+const random = [
+    "/1f/01/001.jpg",
+    "/1f/01/002.jpg",
+    "/1f/01/003.jpg",
+    "/1f/01/004.jpg"
+  ];
+  
+  function randomImg(randomArray) {
+    var random =
+    randomArray[Math.floor(Math.random() * randomArray.length)];
+    console.log(random);
+    return random;
+  }
+  function sentenceGenerator() {
+    var sentence = `<img src="${randomImg(random)}">`;
+    document.querySelector(".random").innerHTML = sentence;
+  }
+  window.setInterval(function() {
+    sentenceGenerator();
+  }, 500);
+  sentenceGenerator();
+  
