a352e7e1b528e7f2cd742f35a5df73d8a1f852ef
∧° ┐ <we.are.pe.hu@gmail.com>
2022-05-11 15:16:49 +0900

    1f

---
+++ book/random.js
@@ -0,0 +1,59 @@
+const random = [
+    "https://64.media.tumblr.com/9c973a0d8716dae0f37cd92c11d981f7/a44391d2dea1d331-8e/s2048x3072/12a6c123e5670e895179f4e28dcf9fff7c5d8978.jpg",
+    "https://64.media.tumblr.com/33b31b91d3b50ea14b2fa04bc1581171/a44391d2dea1d331-15/s2048x3072/45657096086f6a23139f57ba94fd51eac9308031.jpg",
+    "https://64.media.tumblr.com/b58b530e2ee8b228cb1eef090644f0bd/a44391d2dea1d331-b2/s2048x3072/3fa6687efe997772a7e9154160f26691ec2af21f.jpg",
+    "https://64.media.tumblr.com/8ccca4892938f00141aa9d20f2832944/a44391d2dea1d331-1c/s2048x3072/0e83468c42d9dc53273b3274e73a5cd6d35f8b64.jpg",
+    "https://64.media.tumblr.com/2dba89f9fc6166ca96894a491ebb5c58/a44391d2dea1d331-69/s2048x3072/4090810af4cbcac8e67119e3c7ac0a5f93bfe586.jpg",
+    "https://64.media.tumblr.com/4805aa78f1497903e2125fc454604378/a44391d2dea1d331-d7/s2048x3072/395f736c65984bce83f29d9e9d3fe6dd8436e88e.jpg",
+    "https://64.media.tumblr.com/33486e32e2036ef5062bc55137b91fe7/a44391d2dea1d331-99/s2048x3072/f34fb52ee751d3121c3ba2a808d60064ede7867e.jpg",
+    "https://64.media.tumblr.com/073e93da2950c4d057c2815a88cfa4df/a44391d2dea1d331-a8/s2048x3072/f3a1529e2efc3fcf87998bcabd6a182ceed3d2c1.jpg",
+    "https://64.media.tumblr.com/6cd2a30e083110031012a8b7791f559b/855a9fef802ffe2b-51/s540x810/5ffd61ec58b72fe2852186f62c7ed57e68741228.jpg",
+    "https://64.media.tumblr.com/14f77cd6bed66af4bdc89e2b04d4e18c/855a9fef802ffe2b-60/s540x810/0cf6e4c1ba094be27e8ba5d0d9212e204b739a3d.pnj",
+    "https://64.media.tumblr.com/e57eaadf76b9c836bd8a07c0b003cb37/855a9fef802ffe2b-e1/s540x810/bbdbfbe21645a2ae79bf1527d925fc9c16cfd627.pnj",
+    "https://64.media.tumblr.com/68af232d5ea352b8aa5f654fa1df6ec4/855a9fef802ffe2b-0d/s540x810/d573da6cb10e8e38e15b1d8ed228ae1e0cf99305.jpg",
+    "https://64.media.tumblr.com/75f9bcaa666c960c24643566cdb2a2eb/855a9fef802ffe2b-54/s540x810/6ff8d9afa6c50a4097d2459b8ae8b3742b5e1359.jpg",
+    "https://64.media.tumblr.com/721794bbfd6bd0a974e781d161d51a93/855a9fef802ffe2b-92/s640x960/450dc70015d026a1554eba16276483f380e3690a.pnj",
+    "https://64.media.tumblr.com/127e22a556096fe407e513da408598ad/855a9fef802ffe2b-43/s540x810/d0acec18f456b6e87787c47e8d08ecfe48ec26a2.pnj",
+    "https://64.media.tumblr.com/1b3b65c03fbf40de14ba302763f56b16/190e425c884306e5-55/s2048x3072/a8df9a5adaad7acfdf3ccfcd7f875046b7a0820e.jpg",
+    "https://64.media.tumblr.com/96f6b2b0ba75f8ab3edbaf247920b52a/190e425c884306e5-04/s2048x3072/19b67e0e8e5fe188a32fc11247c12be93a18e2ce.jpg",
+    "https://64.media.tumblr.com/c517ff0aeaf92b02a534c9180ff2fbdc/190e425c884306e5-91/s2048x3072/b7adb31ea43efee1880e279c8e3023488193895e.jpg",
+    "https://64.media.tumblr.com/e3d542378b350f0d67867eee46bb8850/190e425c884306e5-5f/s2048x3072/4fdf2701eaaaa33a721c2f54c78a15b0aaacb6cc.jpg",
+    "https://64.media.tumblr.com/98c374a052e1786a82241b819f093252/190e425c884306e5-d5/s2048x3072/6bfd163da73eeb21e7c2121acf50b3e3a6acb4c1.jpg",
+    "https://64.media.tumblr.com/bc980f83281f3a306642132541399731/190e425c884306e5-28/s2048x3072/61dea442f6035e1e44f3a2f5084ac2830c2c4117.jpg",
+    "https://64.media.tumblr.com/bd20caa35b44177c506a83f62625bfa5/190e425c884306e5-3e/s2048x3072/a2ded39ca0c9c9198d07466ad1d2cdc391644b1f.jpg",
+    "https://64.media.tumblr.com/c212c305626549134df836210e247875/190e425c884306e5-16/s2048x3072/373dd4369beb0560250d8b50aacbecedd5a9ce6e.jpg",
+    "https://64.media.tumblr.com/4fe2186ea5f9cc718f6db640a29ea254/033ad72d951cbbf7-79/s2048x3072/558d0a1a540ef1b205be2d14a274069f4ad2506f.jpg",
+    "https://64.media.tumblr.com/52267a1fb792f7724f89ae5dad05f11c/033ad72d951cbbf7-b5/s2048x3072/91143bdae4b72ae5bb42ca231b5bd619c957927d.jpg",
+    "https://64.media.tumblr.com/a597aba8b6861ab21776bd8aa18c6839/033ad72d951cbbf7-e9/s2048x3072/c10b7ffe0c3378d8201f823d72ab9f6e51241f26.jpg",
+    "https://64.media.tumblr.com/b842224e3fd12100fb52a8f6f3773c16/033ad72d951cbbf7-6e/s2048x3072/b62e220586a6394529f26b23de9a4ca2a81bddb7.jpg",
+    "https://64.media.tumblr.com/6d180324466e7a085a2c6fb23942926b/033ad72d951cbbf7-d6/s2048x3072/f6967e548f3277a242e84b25286defd570789032.jpg",
+    "https://64.media.tumblr.com/cd4a94fa1703a5ae32c1a70c0981cb55/033ad72d951cbbf7-9f/s2048x3072/de658a4f973de7d9fbf9cff1fb6e53dfbf86c99a.jpg",
+    "https://64.media.tumblr.com/2dce90e37904a45d83b356c92997eb2f/cf20ece0fefb6662-42/s2048x3072/438dbd5d89a631fc3f7d2989d0fe31c4099fbb85.jpg",
+    "https://64.media.tumblr.com/6d24254236d57c517234a541fa9ee666/cf20ece0fefb6662-e9/s2048x3072/05b83ba18eaf56b1e5b0c6722c4035d943eb432c.jpg",
+    "https://64.media.tumblr.com/383936b470148ac6208ac395f3ca57c4/cf20ece0fefb6662-07/s2048x3072/bba78d7601bd8934c502e76770acc9b2dcffe13c.jpg",
+    "https://64.media.tumblr.com/d6ab47f55aa3489b246cc223bac3b83e/cf20ece0fefb6662-c9/s2048x3072/9e605de9bd6b0bb8947ae026f2ca9f862723fc8e.jpg",
+    "https://64.media.tumblr.com/b42bceb72429aebc5a860f61af5ffdbc/cf20ece0fefb6662-9d/s2048x3072/efe8f38d5c10420ad255da35922e52e4a12f9114.jpg",
+    "https://64.media.tumblr.com/fc7a6f64a226928ef64a2c7278e63de6/cf20ece0fefb6662-74/s2048x3072/e08ca2cd9fcbbb9371a3e293a142b19a1236e552.jpg",
+    "https://64.media.tumblr.com/1b9a105bd394a971789795be17cac377/cf20ece0fefb6662-6c/s2048x3072/6f0b03cbc3e54ee9b8041fab845d91af75bc0106.jpg",
+    "https://64.media.tumblr.com/09c2af9ce3b0838bcfb530dbda0d5f72/31a0c2dc6949b756-1f/s2048x3072/61d9991152239328fb41da09d10ce5d0bd2a8a41.jpg",
+    "https://64.media.tumblr.com/e31cd8d5d59bf2168e4d651bbc5a4f17/31a0c2dc6949b756-4e/s2048x3072/7eafbcf638cbe6980f6db9eee9c5a3f03dc98734.jpg",
+    "https://64.media.tumblr.com/f10a13a7f2886720d91b0889a203d7ab/31a0c2dc6949b756-41/s2048x3072/b599de740330464aa83fdaa5412b72cbf37625d1.jpg",
+    "https://64.media.tumblr.com/ab9761e2dc1a80f9a6aa8ca993df1ed4/31a0c2dc6949b756-10/s2048x3072/b5e84004fbc3e89006e93926c4ff23c8cf6c9681.jpg",
+    "https://64.media.tumblr.com/709659fe59c95658c9d2dc8646503498/31a0c2dc6949b756-ab/s2048x3072/d551ef60cffdfc6d3e96d23e8973e154651b7f7d.jpg"
+];
+
+function randomImg(randomArray) {
+    var random =
+        randomArray[Math.floor(Math.random() * randomArray.length)];
+    console.log(random);
+    return random;
+}
+
+function sentenceGenerator() {
+    var sentence = `<img src="${randomImg(random)}">`;
+    document.querySelector(".random").innerHTML = sentence;
+}
+window.setInterval(function() {
+    sentenceGenerator();
+}, 1000);
+sentenceGenerator();
--- random.js
+++ random.js
@@ -1,23 +1,23 @@
 const random = [
-  "http://vg.pe.hu/1f/01/001.jpg",
-  "http://vg.pe.hu/1f/02/01.jpg",
-  "http://vg.pe.hu/1f/03/000.jpg",
-  "http://vg.pe.hu/1f/04/000.jpeg",
-  "http://vg.pe.hu/1f/05/000.png"
-  ];
-  
-  function randomImg(randomArray) {
+    "http://vg.pe.hu/1f/01/001.jpg",
+    "http://vg.pe.hu/1f/02/01.jpg",
+    "http://vg.pe.hu/1f/03/000.jpg",
+    "http://vg.pe.hu/1f/04/000.jpeg",
+    "http://vg.pe.hu/1f/05/000.png"
+];
+
+function randomImg(randomArray) {
     var random =
-    randomArray[Math.floor(Math.random() * randomArray.length)];
+        randomArray[Math.floor(Math.random() * randomArray.length)];
     console.log(random);
     return random;
-  }
-  function sentenceGenerator() {
+}
+
+function sentenceGenerator() {
     var sentence = `<img src="${randomImg(random)}">`;
     document.querySelector(".random").innerHTML = sentence;
-  }
-  window.setInterval(function() {
+}
+window.setInterval(function() {
     sentenceGenerator();
-  }, 2000);
-  sentenceGenerator();
-  
+}, 2000);
+sentenceGenerator();
