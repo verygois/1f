df4e73002f36603e0a96933d8e3d2e5e84a44174
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2022-11-25 02:44:24 +0900

    vg

--- 01/random.js
+++ 01/random.js
@@ -1,22 +1,21 @@
 const random = [
-    "001.jpg",
-    "002.jpg",
-    "003.jpg",
-    "004.jpg"
-  ];
-  
-  function randomImg(randomArray) {
-    var random =
-    randomArray[Math.floor(Math.random() * randomArray.length)];
-    console.log(random);
-    return random;
-  }
-  function sentenceGenerator() {
-    var sentence = `<img src="${randomImg(random)}">`;
-    document.querySelector(".random").innerHTML = sentence;
-  }
-  window.setInterval(function() {
-    sentenceGenerator();
-  }, 1000);
+  "001.jpg",
+  "002.jpg",
+  "003.jpg",
+  "004.jpg"
+];
+
+function randomImg(randomArray) {
+  var random =
+  randomArray[Math.floor(Math.random() * randomArray.length)];
+  console.log(random);
+  return random;
+}
+function sentenceGenerator() {
+  var sentence = `<img src="${randomImg(random)}">`;
+  document.querySelector(".random").innerHTML = sentence;
+}
+window.setInterval(function() {
   sentenceGenerator();
-  
+}, 1000);
+sentenceGenerator();
