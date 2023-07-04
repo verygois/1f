a1c5f0081900c6b6047e03f8c125959d0078bbcc
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-07-04 17:18:14 +0900

    01

--- random.js
+++ random.js
@@ -1,15 +1,3 @@
-const random = [
-  "01.jpg",
-  "02.jpg",
-  "03.jpg",
-  "04.jpg",
-  "05.jpg",
-  "06.jpg",
-  "07.jpg",
-  "08.jpg",
-  "09.jpg"
-];
-
 function randomImg(randomArray) {
   var random =
     randomArray[Math.floor(Math.random() * randomArray.length)];
