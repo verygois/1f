f91a1b0910cf552d63c630a69d15c8db3472af3d
verygois <sorryforthedelayinsending.vg.pe.hu>
2023-09-10 20:11:25 +0900

    1f

--- book/script.js
+++ book/script.js
@@ -3,6 +3,9 @@
 const directory = "https://ia800505.us.archive.org/20/items/1f_20230910/";
 
 function thisIMG(id, arr) {
-  let ii = 0;
-  document.getElementById(id).style.backgroundImage = `url('${directory}${arr[ii]}'`;
+  arr.forEach((arrEach) => {
+    const crearIMG = document.createElement("img");
+    crearIMG.src = directory + arrEach;
+    document.getElementById(id).appendChild(crearIMG)
+  });
 }
