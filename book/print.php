6f41c3c99a8ef74b642f27bb54028d1aec07c267
verygois <sorryforthedelayinsending.vg.pe.hu>
2023-09-11 13:04:01 +0900

    1f

--- book/print.php
+++ book/print.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set('Asia/Tokyo');
 echo "<p>This Book was published by printing a website.</p>";
 echo "<h2>by <i>".$_SERVER["REMOTE_ADDR"]."</i></h2>";
 echo "<h3>on <span>".date('Y/m/d H:i:s')."</span></h3>";
