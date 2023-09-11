354fa6103c62b57a8ff7f7b3940e976f13d28063
verygois <sorryforthedelayinsending.vg.pe.hu>
2023-09-11 18:42:48 +0900

    1f

--- book/print.php
+++ book/print.php
@@ -1,6 +1,7 @@
 <?php
 date_default_timezone_set('Asia/Tokyo');
-echo "<p>This Book was published by printing a website.</p>";
+echo "<p>This Book was published by WWW to Print.</p>";
 echo "<h2>by <i>".$_SERVER["REMOTE_ADDR"]."</i></h2>";
 echo "<h3>on <span>".date('Y/m/d H:i:s')."</span></h3>";
+echo "<p>Copyright © 2022 Very Gois, All Rights Reserved.</p>"
 ?>
